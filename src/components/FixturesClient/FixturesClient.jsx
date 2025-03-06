ee6cdfc0a39e80b4f80bcf56469516913c408ffc
Danny White <3104761+dnywh@users.noreply.github.com>
2025-03-06 12:34:55 +1000

    Exclude paid tiers

--- src/components/FixturesClient/FixturesClient.jsx
+++ src/components/FixturesClient/FixturesClient.jsx
@@ -11,7 +11,7 @@ export default function FixturesClient() {
   const [showScore, setShowScore] = useState(false);
   const [selectedCompetitions, setSelectedCompetitions] = useState([
     "premier-league",
-    "champions-league",
+    // "champions-league",
   ]);
   const [fixtures, setFixtures] = useState([]);
   const [loading, setLoading] = useState(true); // Start with loading true
@@ -69,30 +69,24 @@ export default function FixturesClient() {
         <h2>Controls</h2>
         <fieldset>
           <legend>Competitions:</legend>
-          {Object.entries(COMPETITIONS).map(([competitionId, competition]) => (
-            <div key={competitionId}>
-              <input
-                type="checkbox"
-                id={competitionId}
-                name={competitionId}
-                disabled={competition.tier === "paid"}
-                checked={selectedCompetitions.includes(competitionId)}
-                onChange={handleCompetitionChange}
-              />
-              <label htmlFor={competitionId}>
-                {competition.name}{" "}
-                {competition.tier === "paid" ? "(paid users only)" : ""}{" "}
-              </label>
-            </div>
-          ))}
+          {Object.entries(COMPETITIONS)
+            .filter(([, competition]) => competition.tier !== "paid")
+            .map(([competitionId, competition]) => (
+              <div key={competitionId}>
+                <input
+                  type="checkbox"
+                  id={competitionId}
+                  name={competitionId}
+                  checked={selectedCompetitions.includes(competitionId)}
+                  onChange={handleCompetitionChange}
+                />
+                <label htmlFor={competitionId}>{competition.name}</label>
+              </div>
+            ))}
         </fieldset>
 
         <Switch checked={showScore} onChange={setShowScore}>
-          {/* className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600" */}
-          <span>
-            {/*  className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" */}
-            Scores {showScore ? "ON" : "OFF"}
-          </span>
+          <span>Scores {showScore ? "ON" : "OFF"}</span>
         </Switch>
       </section>
 
@@ -102,16 +96,13 @@ export default function FixturesClient() {
           <p>Loading fixtures...</p>
         ) : fixtures?.length > 0 ? (
           <ul>
-            {fixtures.map((fixture) => {
-              // console.log(fixture);
-              return (
-                <Fixture
-                  key={fixture.id}
-                  fixture={fixture}
-                  showScore={showScore}
-                />
-              );
-            })}
+            {fixtures.map((fixture) => (
+              <Fixture
+                key={fixture.id}
+                fixture={fixture}
+                showScore={showScore}
+              />
+            ))}
           </ul>
         ) : (
           <p>No fixtures found</p>
