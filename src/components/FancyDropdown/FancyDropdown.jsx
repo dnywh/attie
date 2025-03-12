45a3900710dac30cb67adaddc44d8214d887d412
Danny White <3104761+dnywh@users.noreply.github.com>
2025-03-12 17:29:54 +1000

    Shadow on sticky header scroll

--- src/components/FancyDropdown/FancyDropdown.jsx
+++ src/components/FancyDropdown/FancyDropdown.jsx
@@ -9,7 +9,24 @@ import {
   DialogTitle,
 } from "@headlessui/react";
 import Button from "@/components/Button";
-import { styled } from "@pigment-css/react";
+import { styled, keyframes } from "@pigment-css/react";
+// import {
+//   createCardStyle,
+//   veryBasicCardStyle,
+//   smallCardStyle,
+// } from "@/styles/commonStyles";
+
+const scrollShadow = keyframes({
+  from: {
+    borderBottom: "none",
+    boxShadow: "none",
+  },
+  to: {
+    // Variable defined inside component where theme is accessible
+    borderBottom: `1px solid var(--header-border-color)`,
+    boxShadow: `0 4px 0 0 var(--header-shadow-color)`,
+  },
+});
 
 const DialogHeader = styled("header")(({ theme }) => ({
   display: "flex",
@@ -18,34 +35,53 @@ const DialogHeader = styled("header")(({ theme }) => ({
   // Sticky behaviour
   position: "sticky",
   top: "0",
+  // Sticky styling
   backgroundColor: theme.colors.mid.secondary,
+  padding: "0 0.75rem",
+  minHeight: "3rem", // 48px
+
+  "@supports (animation-timeline: scroll())": {
+    // https://ryanmulligan.dev/blog/sticky-header-scroll-shadow/
+    // https://mskelton.dev/blog/css-scroll-animations
+    "--header-border-color": theme.colors.text,
+    "--header-shadow-color": theme.colors.shadow,
+    animation: `${scrollShadow} linear both`,
+    animationTimeline: "scroll()",
+    animationRange: "0 0.5rem",
+  },
 }));
 
 const StyledButton = styled("button")(({ theme }) => ({
-  appearance: "none",
-  border: "none",
-  cursor: "pointer",
-
   display: "flex",
   gap: "0.65rem",
   padding: "0.65rem",
-  borderColor: theme.colors.text,
-  borderStyle: "solid",
-  borderWidth: "1px",
-  borderRadius: "0.25rem",
+  color: theme.colors.text,
   backgroundColor: theme.colors.foreground,
   alignItems: "center",
   fontSize: "0.85rem",
   fontWeight: "500",
+  lineHeight: "100%",
+  height: "2.5rem", // 40px
   // textWrap: "noWrap",
   overflow: "hidden",
   // whiteSpace: "nowrap",
 
-  transition: "transform 0.1s ease",
-  transformOrigin: "bottom center",
+  // ...createCardStyle({ interactive: true })({ theme }),
+
+  // ...veryBasicCardStyle({ theme }),
+
+  border: `1px solid ${theme.colors.text}`,
+  borderRadius: "2px",
+  boxShadow: `0.5px 1.5px 0 0 ${theme.colors.foreground}`,
+
+  transition: `transform ${theme.curves.springy}`,
+  // transformOrigin: "bottom center",
 
   "&:hover": {
-    transform: "translateY(-2px) scale(1.005)",
+    transform: "translateY(-1.5px) scale(1.005)",
+  },
+  "&:active": {
+    transform: "translateY(0) scale(0.965)",
   },
 
   "&::after": {
@@ -54,6 +90,11 @@ const StyledButton = styled("button")(({ theme }) => ({
     // flexGrow: 1,
     // borderBottom: "1px solid black",
     // margin: "0.5rem 0",
+  },
+
+  "&:focus": {
+    backgroundColor: "blue",
+    opacity: "0.5",
   },
 
   variants: [
@@ -107,7 +148,7 @@ const CountSpan = styled("span")(({ theme }) => ({
   flexShrink: 0,
 }));
 
-// Dialog
+// Dialog styles
 const StyledDialog = styled(Dialog)({
   position: "relative",
   zIndex: "50",
@@ -119,12 +160,12 @@ const DialogInner = styled("div")(({ theme }) => ({
   left: "50%",
   transform: "translateX(-50%)",
   backgroundColor: theme.colors.mid.secondary,
-  padding: "1rem 0.75rem 1.75rem",
+  padding: "0 0 1.75rem",
   width: "calc(100% - 1rem)",
   maxWidth: "30rem",
   borderRadius: "0.25rem",
 
-  maxHeight: "calc(100dvh - 1rem)",
+  maxHeight: "calc(100dvh - 4rem)",
   overflowY: "scroll",
 
   "@media (min-height: 768px)": {
@@ -135,7 +176,9 @@ const DialogInner = styled("div")(({ theme }) => ({
   },
 }));
 
-const StyledDialogPanel = styled(DialogPanel)({
+// Required so padding can be separate from sticky header
+const DialogContent = styled("div")({
+  padding: "0 0.75rem",
   display: "flex",
   flexDirection: "column",
   gap: "1.5rem",
@@ -184,20 +227,20 @@ function FancyDropdown({
       <StyledButton fillSpace={fillSpace} onClick={open}>
         <IconSpan>{icon}</IconSpan>
         {label && <ContentSpan fillSpace={fillSpace}>{label}</ContentSpan>}
-        {count && <CountSpan>{count}</CountSpan>}
+        {count > 0 && <CountSpan>{count > 9 ? "9+" : count}</CountSpan>}
       </StyledButton>
 
       <StyledDialog open={isOpen} onClose={() => setIsOpen(false)}>
         <StyledDialogBackdropOne />
         <StyledDialogBackdropTwo />
         <DialogInner>
-          <StyledDialogPanel transition>
+          <DialogPanel transition>
             <DialogHeader>
               <Button onClick={close}>Close</Button>
               {/* <DialogTitle>Sport and competitions</DialogTitle> */}
             </DialogHeader>
-            {children}
-          </StyledDialogPanel>
+            <DialogContent>{children}</DialogContent>
+          </DialogPanel>
         </DialogInner>
       </StyledDialog>
     </>
